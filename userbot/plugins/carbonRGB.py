6740fea4a0aebe2f8eb9d6113169b6247d04a0ca
The-Avengers-leader <71750431+The-Avengers-leader@users.noreply.github.com>
2020-10-19 10:51:44 +0530

    Update carbonRGB.py

--- userbot/plugins/carbonRGB.py
+++ userbot/plugins/carbonRGB.py
@@ -100,7 +100,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="Carbonised by [Dark-Cobra](https://github.com/DARK-COBRA/DARKCOBRA)",
+         caption="Carbonised by [eliza](https://github.com/The-Avengers-leader/Eliza)",
          force_document=False,
          reply_to=e.message.reply_to_msg_id,
          )
