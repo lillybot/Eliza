e326a651ae16c69380f1e3ccb13e73165098ce65
The-Avengers-leader <71750431+The-Avengers-leader@users.noreply.github.com>
2020-10-19 11:13:04 +0530

    Update power_tools.py

--- userbot/plugins/power_tools.py
+++ userbot/plugins/power_tools.py
@@ -19,7 +19,7 @@ async def _(event):
     await asyncio.sleep(2)
     await event.edit("Restarting [███]...")
     await asyncio.sleep(2)
-    await event.edit("Restarted boss....✈ `.king` me or type `.help` or type '.awake' or type '.allive' to check if your DC is online/alive ")
+    await event.edit("Restarted boss....✈ `.king` me or type `.help` or type '.awake' or type '.alive' to check if your ELIZA is online/alive ")
     await borg.disconnect()
     os.execl(sys.executable, sys.executable, *sys.argv)
     # You probably don't need it but whatever
